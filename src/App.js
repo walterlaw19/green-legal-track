2a7fc2e88f163f8e619c91c561cb334c8c74ee35
SC2021 <walterlaw19@gmail.com>
2022-02-02 23:31:03 -0500

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Foot from './Footer';
+
 
 function App() {
   return (
+    
     <div className="App">
+      <h1>Welcome to my Legal Track</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Green Legal Track <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +19,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          footer should be here
+          <Foot/>
+         
         </a>
       </header>
     </div>
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+
+
+
+function Foot() {
+    return (
+        <div>
+            It must be in Capital for it work
+            WG 2021 coding class
+        </div>
+    );
+}
+
+export default Foot;
